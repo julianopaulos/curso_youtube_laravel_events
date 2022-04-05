9c73c9a552eb8c50112289cdb3cd831a2dc942c4
julianopaulos <julianopaulo.santos@hotmail.com>
2022-04-05 16:05:55 -0300

    adicionando layout principal e apenas puxando content de outros, adicionado bootstrap e google fonts também

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-        <title>Laravel</title>
-        <link rel="stylesheet" href="/css/styles.css">
-        <script src="/js/script.js"></script>
-    </head>
-    <body class="antialiased">
-        {{-- esse é um comentário do blade --}}
-        <h1>{{$nome}}</h1>
-        <img src="/img/banner.jpg"/>
-    </body>
-</html>
+@section('title', 'HDC Events')
+
+@section('content')
+    {{-- esse é um comentário do blade --}}
+    <h1>{{$nome}}</h1>
+    <img src="/img/banner.jpg"/>
+@endsection
