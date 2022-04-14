788e8ed562d996f57c775df87cf7e20ebc1dca32
julianopaulos <julianopaulo.santos@hotmail.com>
2022-04-14 18:18:40 -0300

    adicionado dono do evento na view

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Event;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
@@ -57,7 +58,9 @@ class EventController extends Controller
 
     public function show($id){
         $event = Event::findOrFail($id);
-        return view('events.show', compact('event'));
+        $eventOwner = User::where('id', $event->user_id)->first();
+
+        return view('events.show', compact(['event', 'eventOwner']));
     }
 
     /*public function test($id){
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -20,7 +20,7 @@
                 </p>
                 <p class="event-owner">
                     <ion-icon name="star-outline"></ion-icon>
-                    Dono do evento
+                    {{ $eventOwner->name }}
                 </p>
                 <a href="" class="btn btn-primary" id="event-submit">Confirmar presença</a>
                 <h3>O evento conta com:</h3>
